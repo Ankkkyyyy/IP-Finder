d3e54641689b94cc855c0593f7526be5de241ad7
Ankit Gireesh Mishra <98558150+Ankkkyyyy@users.noreply.github.com>
2022-01-29 04:35:31 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args)
     {
-           new IP_Finder();
+            new IP_Finder();
     }
 }
 //--------------------------------------All Set To Run----------------------------
